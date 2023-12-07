a744dded20e6b527c475be200c958368ca054476
Sofia <savth2004@gmail.com>
2023-12-07 22:48:27 +0600

    add resource files

---
+++ lib/resources/images.dart
@@ -0,0 +1,11 @@
+class Images {
+  static const flag = 'assets/images/flag.png';
+  static const percent = 'assets/images/percent.png';
+  static const rightS = 'assets/images/right_s.png';
+  static const speedometer = 'assets/images/speedometer.png';
+  static const percentWithoutBack = 'assets/images/percent_without_back.png';
+  static const arrowsForwardBack = 'assets/images/arrows_forward_back.png';
+  static const cross = 'assets/images/cross.png';
+  static const arrowRight = 'assets/images/arrow_right.png';
+  static const avatar = 'assets/images/avatar.png';
+}
